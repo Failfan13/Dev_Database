157c769df5127527c421074b4a4c117cc3f21814
Erik Maan <erikmaan02@gmail.com>
2023-09-11 17:20:56 +0200

    Add Lesson 2 practical

---
+++ Practical/Lesson 2/Program.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System.ComponentModel.DataAnnotations;
+class Program
+{
+    public static void Main()
+    {
+
+    }
+}
+
+class myContext : DbContext
+{
+    public DbSet<MyTable> MyTables { get; set; }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseNpgsql(@"Host=localhost:5434;User ID=postgres;Password=;Database=Lesson2db;Maximum Pool Size=200");
+    }
+}
+
+class MyTable
+{
+    [Key]
+    public int ID { get; set; }
+    public string Name { get; set; }
+}
